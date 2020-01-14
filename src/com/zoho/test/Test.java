a2cb41d0cd2f848ca394c04d7a73baa766f295ea
sugheerth <42635206+sugheerth@users.noreply.github.com>
2020-01-14 12:43:48 +0530

    adding Test.java

---
+++ src/com/zoho/test/Test.java
@@ -0,0 +1,16 @@
+package com.zoho.test;
+
+public class Test{
+
+  public static void main(String args[]){
+  
+    String a = "Hello";
+    String b = "world";
+    System.out.println(a+ " " +b);
+    
+    String a = "Hello";
+    String b = "world";
+    System.out.println(a+ " " +b);
+    
+  }
+}
